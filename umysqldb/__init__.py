bb83ba50c3089ddfe6b64d9170151a67438527bb
Sean Talts <sean@lot18.com>
2012-07-23 17:57:30 -0400

    remove two more relative imports

--- umysqldb/__init__.py
+++ umysqldb/__init__.py
@@ -4,13 +4,13 @@ from pymysql import *
 from pymysql import DATETIME
 from pymysql import __all__
 
-from .util import setdocstring
+from util import setdocstring
 
 __all__ += ['DATETIME']
 
 @setdocstring(pymysql.Connect)
 def Connect(*args, **kwargs):
-    from .connections import Connection
+    from connections import Connection
     return Connection(*args, **kwargs)
 
 def thread_safe():
